e2a530cb712c345832992342f260c257d96f59a1
rafaapcode <rafaap2003@gmail.com>
2022-11-15 19:56:58 -0300

    Message for error

--- src/middlewares/erros/RestaurantErrors.js
+++ src/middlewares/erros/RestaurantErrors.js
@@ -5,7 +5,7 @@ class RestaurantValidation {
     const { error } = RestaurantValidations.restaurantBody(req.body);
 
     if (error) {
-      return res.status(400).json({ message: 'Data is not valid.' });
+      return res.status(400).json({ message: 'Name must be at least 7 characters. Address must be at least 5 characters. Hour must be at least 10 characters.' });
     }
 
     return next();
